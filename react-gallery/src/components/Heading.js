60c2c58b881579e98b8f302374b56e034062cabc
Chivisto <isaaccovarr@gmail.com>
2022-04-29 14:59:54 -0600

    Added Text

--- react-gallery/src/components/Heading.js
+++ react-gallery/src/components/Heading.js
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from 'react';
+import styled from 'styled-components';
+
+const Header = styled.header`
+  max-width: 70rem;
+  margin: 2rem auto;
+  text-align: center;
+`;
+
+const H1 = styled.h1`
+  font-family: 'Oswald', sans-serif;
+  margin-bottom: 1em;
+`;
 
 export const Heading = () => {
   return (
-    <div>
-        <h1>Heading</h1>
-    </div>
+    <Header>
+      <H1>Unsplash</H1>
+      <p>React Image Gallery from API</p>
+      <p>Isaac Covarrubias</p>
+    </Header>
   )
 }
