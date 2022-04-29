375c7e5efe58216ce8ad30ce7850c5cf1ab840c1
Chivisto <isaaccovarr@gmail.com>
2022-04-29 14:54:47 -0600

    Added Styles

--- react-gallery/src/App.js
+++ react-gallery/src/App.js
@@ -1,9 +1,35 @@
-import React, {useState, useEffect} from "react";
-import {Heading} from './components/Heading'
-import {Loader} from './components/Loader'
-import {UnsplashImage} from './components/UnsplashImage'
+import React, {useState, useEffect} from 'react';
+import {Heading} from './components/Heading';
+import {Loader} from './components/Loader';
+import {UnsplashImage} from './components/UnsplashImage';
 
-import axios from "axios";
+
+import axios from 'axios';
+import styled from 'styled-components';
+import {createGlobalStyle} from 'styled-components';
+
+//Style
+const GlobalStyle = createGlobalStyle`
+  *{
+    margin: 0px;
+    padding: 0px;
+    box-sizing: border-box;
+  }
+
+  body{
+    font-family: sans-serif;
+  }
+`;
+
+const WrapperImg = styled.section`
+  max-width: 70rem;
+  margin: 4rem auto;
+  display: grid;
+  grid-gap: 1em;
+  grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
+  grid-auto-rows: 300px;
+`;
+
 
 function App() {
   const [images, setImages] = useState([]);
@@ -19,10 +45,13 @@ function App() {
   return (
     <div className="App">
       <Heading />
+      <GlobalStyle />
       <Loader />
-      {images.map(image => (
-        <UnsplashImage url={image.urls.thumb} key={image.id} />
-      ))}
+      <WrapperImg>
+        {images.map(image => (
+          <UnsplashImage url={image.urls.thumb} key={image.id} />
+        ))}
+      </WrapperImg>
     </div>
   );
 }
